df1acf89fa6f0b8e61e43c0040248a449719249f
David Lee <seekdavidlee@gmail.com>
2025-02-23 18:35:04 -0600

    add API_LISTEN_PORT to listen on that port

--- src/Program.cs
+++ src/Program.cs
@@ -4,6 +4,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+var apiListenPortStr = Environment.GetEnvironmentVariable("API_LISTEN_PORT");
+if (apiListenPortStr is not null && int.TryParse(apiListenPortStr, out var apiListenPort))
+{
+    builder.WebHost.ConfigureKestrel(serverOptions =>
+    {
+        serverOptions.ListenAnyIP(apiListenPort);
+    });
+}
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
